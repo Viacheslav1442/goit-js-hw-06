3c5cb3bc938bed3e5e31be169a4b9098ce005572
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-04-10 20:31:03 +0300

    js

--- js/task-2.js
+++ js/task-2.js
@@ -1,59 +1,40 @@
-const getUsersWithFriend = (users, friendName) =>
-    users.filter(user => user.friends.includes(friendName));
-
-const allUsers = [
-    {
-        name: "Moore Hensley",
-        friends: ["Sharron Pace"]
-    },
-    {
-        name: "Sharlene Bush",
-        friends: ["Briana Decker", "Sharron Pace"]
-    },
-    {
-        name: "Ross Vazquez",
-        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-    },
-    {
-        name: "Elma Head",
-        friends: ["Goldie Gentry", "Aisha Tran"]
-    },
-    {
-        name: "Carey Barr",
-        friends: ["Jordan Sampson", "Eddie Strong"]
-    },
-    {
-        name: "Blackburn Dotson",
-        friends: ["Jacklyn Lucas", "Linda Chapman"]
-    },
-    {
-        name: "Sheree Anthony",
-        friends: ["Goldie Gentry", "Briana Decker"]
+class Storage {
+    // Конструктор приймає масив товарів
+    constructor(initialItems) {
+        this.#items = initialItems; // Приватна властивість для зберігання товарів
     }
-];
 
-console.log(getUsersWithFriend(allUsers, "Briana Decker"));
-// [
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+    // Приватна властивість
+    #items;
 
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// [
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+    // Метод для отримання поточного списку товарів
+    getItems() {
+        return this.#items;
+    }
 
-console.log(getUsersWithFriend(allUsers, "Adrian Cross")); // []
+    // Метод для додавання товару до списку
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    // Метод для видалення товару з списку
+    removeItem(itemToRemove) {
+        const index = this.#items.indexOf(itemToRemove);
+        if (index !== -1) {
+            this.#items.splice(index, 1);
+        }
+    }
+}
+
+// Ініціалізація об'єкта та перевірка роботи методів
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
